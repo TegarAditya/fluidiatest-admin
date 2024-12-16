4064530eb615d456e19d1b6d0aec5e3b6fe491a3
Tegar Aditya <tegaraditya53@gmail.com>
2024-12-16 13:09:57 +0700

    Add helper functions for generating public IDs and retrieving test type options

---
+++ app/helpers.php
@@ -0,0 +1,33 @@
+<?php
+
+use Hidehalo\Nanoid\Client;
+
+if (! function_exists('generatePublicId')) {
+    /**
+     * Generates a public ID using the specified alphabet and size.
+     *
+     * @param string $alphabet The set of characters to use for generating the ID. Default is '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.
+     * @param int $size The length of the generated ID. Default is 21.
+     * @return string The generated public ID.
+     */
+    function generatePublicId($alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz', $size = 21): string
+    {
+        return (new Client())->formattedId($alphabet, $size);
+    }
+}
+
+if (! function_exists('getTestTypeOption')) {
+    /**
+     * Returns the test type option based on the specified value.
+     *
+     * @param string $value The value to get the test type option for.
+     * @return string The test type option.
+     */
+    function getTestTypeOption(): array
+    {
+        return [
+            '1' => 'Post-test',
+            '2' => 'Pre-test',
+        ];
+    }
+}
