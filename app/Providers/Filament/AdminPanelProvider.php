d00528c21817550354e63a84357163c624effe58
Tegar Aditya <tegaraditya53@gmail.com>
2024-12-19 20:42:36 +0700

    Add database transactions and unsaved changes alerts to Admin Panel configuration

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -38,6 +38,8 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->brandName('Fluidiatest Admin')
             ->sidebarCollapsibleOnDesktop()
+            ->databaseTransactions()
+            ->unsavedChangesAlerts()
             ->discoverResources(in: app_path('Filament/Admin/Resources'), for: 'App\\Filament\\Admin\Resources')
             ->discoverPages(in: app_path('Filament/Admin/Pages'), for: 'App\\Filament\\Admin\Pages')
             ->pages([
