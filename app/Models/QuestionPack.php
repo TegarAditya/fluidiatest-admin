f9537087d28fa36ff76d7d94336127b8081f3d05
Tegar Aditya <tegaraditya53@gmail.com>
2024-12-19 20:42:32 +0700

    Update QuestionPack model to include 'description' and 'is_active' fields; add relationship for QuestionPackQuestionBank

--- app/Models/QuestionPack.php
+++ app/Models/QuestionPack.php
@@ -6,10 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class QuestionPack extends Model
 {
-    protected $fillable = ['code', 'question'];
+    protected $fillable = ['code', 'description', 'is_active'];
 
     public function questions()
     {
         return $this->belongsToMany(QuestionBank::class, 'question_pack_question_bank', 'question_pack_id', 'question_bank_id');
+    }
+
+    public function questionPackQuestionBank()
+    {
+        return $this->hasMany(QuestionPackQuestionBank::class);
     }
 }
