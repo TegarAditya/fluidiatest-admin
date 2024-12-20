f04e579067d405ea9ae4660204ab77a103c9425d
Tegar Aditya <tegaraditya53@gmail.com>
2024-12-20 11:24:49 +0700

    Reorder feedback score options in FeedbacksRelationManager form

--- app/Filament/Admin/Resources/QuestionBankResource/RelationManagers/FeedbacksRelationManager.php
+++ app/Filament/Admin/Resources/QuestionBankResource/RelationManagers/FeedbacksRelationManager.php
@@ -24,10 +24,10 @@ class FeedbacksRelationManager extends RelationManager
                     ->label('Skor')
                     ->required()
                     ->options([
-                        1 => '1',
-                        2 => '2',
-                        3 => '3',
                         4 => '4',
+                        3 => '3',
+                        2 => '2',
+                        1 => '1',
                     ])
                     ->columnSpanFull(),
                 Forms\Components\MarkdownEditor::make('feedback')
