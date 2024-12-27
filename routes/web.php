793bcf6b89283ee7f813c88380f30c8b9fbe05bf
Tegar Aditya <tegaraditya53@gmail.com>
2024-12-27 23:24:26 +0700

    Refactor landing page route to use Livewire component

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Livewire\LandingPage;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('livewire.landing-page');
-});
+Route::get('/', LandingPage::class);
