f23025369cb76969e1dd7a6c929ab7687a82f059
Tegar Aditya <tegaraditya53@gmail.com>
2024-12-27 08:58:22 +0700

    Update landing page layout and add introduction section for improved content presentation

--- resources/views/livewire/landing-page.blade.php
+++ resources/views/livewire/landing-page.blade.php
@@ -27,7 +27,7 @@
                     <div>
                         <ul class="flex font-semibold gap-4">
                             <li class="hover:font-semibold"><a href="#about">Tentang</a></li>
-                            <li class="hover:font-semibold"><a href="#">Profil</a></li>
+                            <li class="hover:font-semibold"><a href="#introduction">Profil</a></li>
                             <li class="hover:font-semibold"><a href="#">Tutorial</a></li>
                         </ul>
                     </div>
@@ -42,7 +42,8 @@
             </div>
         </nav>
         <main>
-            <div class="w-full flex flex-col items-center justify-center min-h-[100dvh] gap-7 hero">
+
+            <section class="w-full flex flex-col items-center justify-center min-h-[100dvh] gap-7 hero">
                 <div class="text-white">
                     <h1 class="font-bold text-5xl text-center md:text-7xl">Welcome to Fluidiatest</h1>
                 </div>
@@ -53,8 +54,123 @@
                         </div>
                     </a>
                 </div>
-            </div>
-            <div id="about" class="w-full bg-white min-h-[100dvh]"></div>
+            </section>
+
+            <section id="about" class="text-gray-600 pt-20 body-font">
+                <div class="container px-5 py-24 mx-auto">
+                    <div class="flex flex-wrap w-full mb-20 flex-col items-center text-center">
+                        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Tentang Fluidiatest</h1>
+                        <p class="lg:w-1/2 w-full leading-relaxed text-gray-500">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table.</p>
+                    </div>
+                    <div class="flex flex-wrap -m-4">
+                        <div class="xl:w-1/3 md:w-1/2 p-4">
+                            <div class="border border-gray-200 p-6 rounded-lg">
+                                <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+                                        <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                                    </svg>
+                                </div>
+                                <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Shooting Stars</h2>
+                                <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
+                            </div>
+                        </div>
+                        <div class="xl:w-1/3 md:w-1/2 p-4">
+                            <div class="border border-gray-200 p-6 rounded-lg">
+                                <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+                                        <circle cx="6" cy="6" r="3"></circle>
+                                        <circle cx="6" cy="18" r="3"></circle>
+                                        <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
+                                    </svg>
+                                </div>
+                                <h2 class="text-lg text-gray-900 font-medium title-font mb-2">The Catalyzer</h2>
+                                <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
+                            </div>
+                        </div>
+                        <div class="xl:w-1/3 md:w-1/2 p-4">
+                            <div class="border border-gray-200 p-6 rounded-lg">
+                                <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+                                        <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                                        <circle cx="12" cy="7" r="4"></circle>
+                                    </svg>
+                                </div>
+                                <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Neptune</h2>
+                                <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
+                            </div>
+                        </div>
+                        <div class="xl:w-1/3 md:w-1/2 p-4">
+                            <div class="border border-gray-200 p-6 rounded-lg">
+                                <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+                                        <path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1zM4 22v-7"></path>
+                                    </svg>
+                                </div>
+                                <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Melanchole</h2>
+                                <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
+                            </div>
+                        </div>
+                        <div class="xl:w-1/3 md:w-1/2 p-4">
+                            <div class="border border-gray-200 p-6 rounded-lg">
+                                <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+                                        <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z"></path>
+                                    </svg>
+                                </div>
+                                <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Bunker</h2>
+                                <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
+                            </div>
+                        </div>
+                        <div class="xl:w-1/3 md:w-1/2 p-4">
+                            <div class="border border-gray-200 p-6 rounded-lg">
+                                <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+                                        <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+                                    </svg>
+                                </div>
+                                <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Ramona Falls</h2>
+                                <p class="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waist co, subway tile poke farm.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+                </div>
+            </section>
+
+            <section id="introduction" class="text-gray-600 pt-20 body-font">
+                <div class="container px-5 py-24 mx-auto flex flex-col">
+                    <div class="lg:w-4/6 mx-auto">
+                        <div class="flex w-full justify-center">
+                            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Profil Pengembang</h1>
+                        </div>
+                        <div class="flex flex-col sm:flex-row mt-10">
+                            <div class="sm:w-1/3 text-center sm:pr-8 sm:py-8">
+                                <div class="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
+                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
+                                        <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                                        <circle cx="12" cy="7" r="4"></circle>
+                                    </svg>
+                                </div>
+                                <div class="flex flex-col items-center text-center justify-center">
+                                    <h2 class="font-medium title-font mt-4 text-gray-900 text-lg">Nurdiyanti, S.Pd.</h2>
+                                    <div class="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
+                                    <p class="text-base">nurdiyanti.2023@student.uny.ac.id</p>
+                                    <p class="text-base">NIM. 23031240018</p>
+                                </div>
+                            </div>
+                            <div class="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
+                                <p class="leading-relaxed text-lg mb-4">Nurdiyanti yang biasa disapa Yayan merupakan perempuan kelahiran Purbalingga, 15 Januari 2000. Pengembang memulai pendidikan tingginya di program Pendidikan Fisika Universitas Sebelas Maret Surakarta. Saat ini pengembang tengah menyelesaikan studi Magister di Pendidikan Fisika Universitas Negeri Yogyakarta. Pengembang memiliki ketertarikan pada bidang asesmen pembelajaran fisika dan pengembangan media pembelajaran. Selain itu, pengembang juga aktif dalam bidang kepenulisan dan kegiatan sosial masyarakat dengan menginisiasi platform Dangau Mucal.</p>
+                                <a class="text-indigo-500 inline-flex items-center">Learn More
+                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                                    </svg>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
         </main>
         <footer class="text-gray-600 body-font">
             <div class="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
