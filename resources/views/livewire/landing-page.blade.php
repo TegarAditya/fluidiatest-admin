55739f5cae1e9abaa047dc7dc962f2eb7bf66487
Tegar Aditya <tegaraditya53@gmail.com>
2024-12-29 19:12:33 +0700

    Enhance landing page with toggleable developer and supervisor sections; add details for supervisor Dr. Drs. Supahar, M.Si

--- resources/views/livewire/landing-page.blade.php
+++ resources/views/livewire/landing-page.blade.php
@@ -96,11 +96,20 @@
     <!-- Start Introduction -->
     <section id="introduction" class="text-gray-600 body-font">
         <div class="container px-5 py-24 mx-auto flex flex-col">
-            <div class="lg:w-4/6 mx-auto">
+            <div x-data="{ open: false }" class="lg:w-4/6 mx-auto">
                 <div class="flex w-full justify-center">
                     <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Profil Pengembang</h1>
                 </div>
-                <div class="flex flex-col sm:flex-row mt-10">
+                <div class="flex w-full justify-center gap-7 items-center mt-7">
+                    <button x-on:click="open = false" x-bind:class="! open ? 'font-semibold' : ''">
+                        <p>Pengembang</p>
+                    </button>
+                    <button x-on:click="open = true" x-bind:class="open ? 'font-semibold' : ''">
+                        <p>Dosen Pembimbing</p>
+                    </button>
+                </div>
+                <!-- Pengembang -->
+                <div x-bind:class="open ? 'hidden' : ''" class="flex flex-col sm:flex-row mt-5">
                     <div class="sm:w-1/3 text-center sm:pr-8 sm:py-8">
                         <div class="w-40 h-40 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
                             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-20 h-20" viewBox="0 0 24 24">
@@ -117,6 +126,31 @@
                     </div>
                     <div class="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
                         <p class="leading-relaxed text-lg mb-4">Nurdiyanti yang biasa disapa Yayan merupakan perempuan kelahiran Purbalingga, 15 Januari 2000. Pengembang memulai pendidikan tingginya di program Pendidikan Fisika Universitas Sebelas Maret Surakarta. Saat ini pengembang tengah menyelesaikan studi Magister di Pendidikan Fisika Universitas Negeri Yogyakarta. Pengembang memiliki ketertarikan pada bidang asesmen pembelajaran fisika dan pengembangan media pembelajaran. Selain itu, pengembang juga aktif dalam bidang kepenulisan dan kegiatan sosial masyarakat dengan menginisiasi platform Dangau Mucal.</p>
+                        <a class="text-indigo-500 inline-flex items-center">Learn More
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                                <path d="M5 12h14M12 5l7 7-7 7"></path>
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+                <!-- Dosen Pembimbing -->
+                <div x-bind:class="! open ? 'hidden' : ''" class="flex flex-col sm:flex-row mt-5">
+                    <div class="sm:w-1/3 text-center sm:pr-8 sm:py-8">
+                        <div class="w-40 h-40 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
+                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-20 h-20" viewBox="0 0 24 24">
+                                <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                                <circle cx="12" cy="7" r="4"></circle>
+                            </svg>
+                        </div>
+                        <div class="flex flex-col items-center text-center justify-center">
+                            <h2 class="font-medium title-font mt-4 text-gray-900 text-lg">Dr. Drs. Supahar, M.Si</h2>
+                            <div class="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
+                            <p class="text-base">supahar@uny.ac.id</p>
+                            <p class="text-base">NIP. 196803151994121001</p>
+                        </div>
+                    </div>
+                    <div class="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
+                        <p class="leading-relaxed text-lg mb-4">Dr. Drs. Supahar, M.Si adalah dosen Departemen Pendidikan Fisika, Fakultas Matematika dan Ilmu Pengetahuan Alam dan Departemen Penelitian dan Evaluasi Pendidikan, Sekolah Pascasarjana, Universitas Negeri Yogyakarta. Penelitiannya berfokus pada pendidikan fisika, pengukuran dan asesmen. </p>
                         <a class="text-indigo-500 inline-flex items-center">Learn More
                             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
                                 <path d="M5 12h14M12 5l7 7-7 7"></path>
