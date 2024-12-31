0aa1a4d9ff55c4abc20caccb11fad268156dd3c3
Tegar Aditya <tegaraditya53@gmail.com>
2024-12-31 14:07:39 +0700

    Add @tailwindcss/typography plugin to Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,5 +17,8 @@ export default {
             },
         },
     },
-    plugins: [require("tailwindcss-animated")],
+    plugins: [
+        require("@tailwindcss/typography"),
+        require("tailwindcss-animated"),
+    ],
 };
