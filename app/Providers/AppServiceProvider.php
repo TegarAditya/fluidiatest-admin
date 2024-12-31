e0e66de009491337063af838ce932c23fd2143ef
Tegar Aditya <tegaraditya53@gmail.com>
2024-12-31 14:08:13 +0700

    Register KaTeX CSS and JS assets in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Filament\Support\Assets\Css;
+use Filament\Support\Assets\Js;
+use Filament\Support\Facades\FilamentAsset;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -23,5 +26,10 @@ class AppServiceProvider extends ServiceProvider
         if (env('FORCE_HTTPS', false)) {
             URL::forceScheme('https');
         }
+
+        FilamentAsset::register([
+            Css::make('katex-css', 'https://cdn.jsdelivr.net/npm/katex@0.16.19/dist/katex.min.css'),
+            Js::make('katex-js', 'https://cdn.jsdelivr.net/npm/katex@0.16.19/dist/katex.min.js'),
+        ]);
     }
 }
