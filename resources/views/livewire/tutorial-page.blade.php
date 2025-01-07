82f5b99ea681ed9f9e99ec1c9ece7a40f984b91f
Tegar Aditya <tegaraditya53@gmail.com>
2025-01-07 21:35:25 +0700

    Update TutorialPage styling and iframe attributes for improved presentation

--- resources/views/livewire/tutorial-page.blade.php
+++ resources/views/livewire/tutorial-page.blade.php
@@ -1,7 +1,7 @@
 <main>
     <div class="min-h-[100dvh] w-full flex items-center justify-center pt-24">
-        <div class="overflow-hidden rounded-lg shadow-2xl border">
-            <iframe loading="lazy" class="w-[80dvw] aspect-video mx-auto"
+        <div class="overflow-hidden rounded-lg shadow-2xl border bg-white">
+            <iframe class="w-[80dvw] aspect-video mx-auto"
                 src="https://www.canva.com/design/DAGarr8oueo/gYmUJmT0YBg1vbRxesJU6g/view?embed" allowfullscreen="allowfullscreen" allow="fullscreen">
             </iframe>
         </div>
