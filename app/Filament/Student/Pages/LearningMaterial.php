eee468c17d953b1ae1aa8620a7ff3f4d44ab83c0
Tegar Aditya <tegaraditya53@gmail.com>
2025-01-08 07:01:44 +0700

    Update S3 URL retrieval in LearningMaterial page to use config settings

--- app/Filament/Student/Pages/LearningMaterial.php
+++ app/Filament/Student/Pages/LearningMaterial.php
@@ -19,6 +19,6 @@ class LearningMaterial extends Page
     {
         $this->learningMaterial = Material::all();
 
-        $this->s3Url = env('AWS_ENDPOINT') . '/' . env('AWS_BUCKET');
+        $this->s3Url = config('filesystems.disks.s3.endpoint') . '/' . config('filesystems.disks.s3.bucket');
     }
 }
