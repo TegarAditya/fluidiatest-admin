279fae5c63a68dbbae7f779f58a350613d986ca3
Tegar Aditya <tegaraditya53@gmail.com>
2025-02-18 15:46:23 +0700

    Update page to use user relationship

--- app/Filament/Admin/Resources/QuestionPackResource/Pages/ResultQuestionPack.php
+++ app/Filament/Admin/Resources/QuestionPackResource/Pages/ResultQuestionPack.php
@@ -14,8 +14,10 @@ use Filament\Tables\Table;
 class ResultQuestionPack extends Page implements HasTable
 {
     use InteractsWithTable;
-    
+
     protected static string $resource = QuestionPackResource::class;
+
+    protected static ?string $title = 'Hasil Ujian';
 
     protected static string $view = 'filament.admin.resources.question-pack-resource.pages.view-result';
 
@@ -24,20 +26,33 @@ class ResultQuestionPack extends Page implements HasTable
         return $table
             ->query(ExamAttempt::query())
             ->columns([
-                Tables\Columns\TextColumn::make('student.name')
-                    ->default(fn ($record) => User::find($record->user_id)->name)
-                    ->label('Nama Siswa'),
-                Tables\Columns\TextColumn::make('student.email')
-                    ->default(fn ($record) => User::find($record->user_id)->email)
-                    ->label('Email'),
+                Tables\Columns\TextColumn::make('user.name')
+                    ->label('Nama Siswa')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('user.email')
+                    ->label('Email')
+                    ->sortable()
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('user.school.name')
+                    ->label('Sekolah')
+                    ->sortable()
+                    ->searchable(),
                 Tables\Columns\TextColumn::make('attempt_id')
-                    ->label('ID Percobaan'),
+                    ->label('ID Percobaan')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label('Tanggal Submit')
+                    ->sortable()
+                    ->date(),
             ])
             ->filters([
                 // ...
             ])
             ->actions([
-                // ...
+                Tables\Actions\Action::make('Lihat Hasil')
+                    ->button()
+                    ->icon('heroicon-o-eye')
+                    ->color('info')
             ])
             ->bulkActions([
                 // ...
