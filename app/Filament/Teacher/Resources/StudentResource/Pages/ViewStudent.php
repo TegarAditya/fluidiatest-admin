7056d9be12150d2eac7337ef8dbfdd86a0474064
Tegar Aditya <tegaraditya53@gmail.com>
2025-02-20 15:26:37 +0700

    Add ViewStudent page for displaying student details in StudentResource

---
+++ app/Filament/Teacher/Resources/StudentResource/Pages/ViewStudent.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Teacher\Resources\StudentResource\Pages;
+
+use App\Filament\Teacher\Resources\StudentResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewStudent extends ViewRecord
+{
+    protected static string $resource = StudentResource::class;
+}
