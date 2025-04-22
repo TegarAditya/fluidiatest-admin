cb3378c8abd0c3540e9e55479ceffa8d07ffcd12
Tegar Aditya <tegaraditya53@gmail.com>
2025-04-22 10:37:35 +0700

    Add iframe to ResultPage for displaying external spreadsheet

--- resources/views/filament/admin/pages/result-page.blade.php
+++ resources/views/filament/admin/pages/result-page.blade.php
@@ -1,3 +1,3 @@
 <x-filament-panels::page>
-
+    <iframe width="100%" height="650px" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQqcBUfOfonqzELdCF2lhpF7ftSjByvrGkX7Pv02acNavxhUhR-DUu25fvI7ndSYMy29zhxx_8tnIln/pubhtml?widget=true&amp;headers=false"></iframe>
 </x-filament-panels::page>
