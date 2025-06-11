bbbb740b2308fcea7a2a0a7b2d27bf12892280d4
Tegar Aditya <tegaraditya53@gmail.com>
2025-06-11 19:14:51 +0700

    Add default learning cover image and update image source in learning material view

--- resources/views/filament/student/pages/learning-material.blade.php
+++ resources/views/filament/student/pages/learning-material.blade.php
@@ -3,7 +3,7 @@
         @foreach($learningMaterial as $materi)
         <div class="relative flex flex-col bg-white shadow-sm border border-slate-200 rounded-lg">
             <div class="relative aspect-video m-2.5 overflow-hidden text-white rounded-md">
-                <img src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80" alt="card-image" />
+                <img src="{{ asset('images/default-learning-cover.jpg') }}" alt="card-image" />
             </div>
             <div class="p-4">
                 <h6 class="mb-2 text-slate-800 text-xl font-semibold">{{ $materi->title }}</h6>
