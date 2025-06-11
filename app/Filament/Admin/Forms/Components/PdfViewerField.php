66dc62b3bd94755f06d9bd998043bd463da9fa02
Tegar Aditya <tegaraditya53@gmail.com>
2025-06-11 20:17:52 +0700

    Add PdfViewerField component for enhanced PDF viewing capabilities

---
+++ app/Filament/Admin/Forms/Components/PdfViewerField.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Filament\Admin\Forms\Components;
+
+use Faker\Provider\Base;
+use Joaopaulolndev\FilamentPdfViewer\Forms\Components\PdfViewerField as BasePdfViewerField;
+use Closure;
+
+class PdfViewerField extends BasePdfViewerField
+{
+    protected string|Closure|null $disk = 's3';
+
+    protected string $minHeight = '80svh';
+}
